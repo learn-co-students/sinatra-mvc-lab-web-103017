d461512ea18c155e0a91966c62ad8c2fcc1d4f46
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-20 22:24:26 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,39 @@
+class PigLatinizer
+  attr_accessor :text
+  # def initialize
+  #   @text = text.split(' ')
+  # end
+
+  def piglatinize(word)
+    vowels = ["a","e","i","o","u"]
+    consonants = ("a".."z").to_a - vowels
+    if vowels.include?(word.downcase[0])
+      latin_word = word + "way"
+      return latin_word
+    else
+      parts = word.split(/([aeiou].*)/)
+      latin_word =  parts[1] + parts[0] + "ay"
+      return latin_word
+    end
+  end
+
+
+   def to_pig_latin(phrase)
+    phrase = phrase.split(' ')
+    latinized = []
+    vowels = ["a","e","i","o","u"]
+    phrase.each do |word|
+
+      if vowels.include?(word.downcase[0])
+        latinized << word + "way"
+      else
+        parts = word.split(/([aeiou].*)/)
+        latinized << parts[1] + parts[0] + "ay"
+      end
+
+    end
+    latinized.join(' ')
+  end
+
+
+end
